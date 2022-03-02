367e1858141419f431067ed6473bc58334955060
Habeeb Salau <46508840+hsalau5@users.noreply.github.com>
2022-03-01 22:28:52 -0500

    Modifying changes from group

--- src/wall_follower.py
+++ src/wall_follower.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python2
 
 import numpy as np
+from scipy import stats
 
 import rospy
 from rospy.numpy_msg import numpy_msg
 from sensor_msgs.msg import LaserScan
 from ackermann_msgs.msg import AckermannDriveStamped
+
+from visualization_tools import *
 
 class WallFollower:
     # Import ROS parameters from the "params.yaml" file.
@@ -16,24 +19,24 @@ class WallFollower:
     SIDE = rospy.get_param("wall_follower/side")
     VELOCITY = rospy.get_param("wall_follower/velocity")
     DESIRED_DISTANCE = rospy.get_param("wall_follower/desired_distance")
-    MAX_STEERING = rospy.get_param("wall_follower/max_steering")
-    K_P = rospy.get_param("wall_follower/k_p")
+    MAX_STEERING_ANGLE = rospy.get_param("wall_follower/max_steering_angle")
+    WALL_TOPIC = "/wall"
+    K_P = SIDE * 2.5
     K_I = rospy.get_param("wall_follower/k_i")
     K_D = rospy.get_param("wall_follower/k_d")
 
     def __init__(self):
-        # TODO:
-        self.error_history = list()
-        # Initialize your publishers and
-        # subscribers here
+        self.error_history = [0] # Initialize error history to 0
 
         # Publihser 
-        self.pub = rospy.Publisher(self.DRIVE_TOPIC, AckermannDriveStamped) 
-
+        self.pub = rospy.Publisher(self.DRIVE_TOPIC, AckermannDriveStamped, queue_size=60)
 
         # Subscriber # TODO: Set up subscriber + callback function
-        rospy.init_node('simple_subscriber', anonymous=True)
-        rospy.Subscriber("my_random_float", self.SCAN_TOPIC, self.drive)
+        rospy.Subscriber(self.SCAN_TOPIC, numpy_msg(LaserScan), self.drive)
+
+        # Line visualizer
+        self.line_pub = rospy.Publisher(self.WALL_TOPIC, Marker, queue_size=1)
+
     
     def pid(self, error, k_p, k_i, k_d):
         response = k_p*error + k_i*sum(self.error_history) + k_d*(error - self.error_history[-1]) 
@@ -41,37 +44,91 @@ class WallFollower:
 
         return response
 
-    def find_position(self, scan, theta_start, theta_end):
+    def find_angle(self,ranges, angles):
+        '''
+        Finds the angle of the wall
+        '''
+        x = np.multiply(ranges, np.cos(angles))
+        y = np.multiply(ranges, np.sin(angles))
+
+        m, b, r_value, p_value, std_err = stats.linregress(x,y)
+
+        return m
+
+    def find_position(self, ranges, angles):
         '''
         Determine y(t)
         Finds the position signal in either distance or angle
         '''
-        # know where to start and end range
-        start_index = int(theta_start - scan.angle_min /scan.angle_increment) # TODO: implement theta_start - scan.angle_min
-        end_index = int(theta_end - scan.angle_min /scan.angle_increment) # TODO: implement theta_end - scan.angle_min
+        # x = np.multiply(ranges, np.cos(angles))
+        # y = np.multiply(ranges, np.sin(angles))
 
-        # slice the scan data to get the range at theta_start and theta_end
-        ranges = scan.ranges[start_index:end_index]
+        
+        # y_ = np.linspace(0., 5., num=20)
+        # x_ = np.multiply(0, y_)
 
-        #get (x,y) coordinates for each position
-        x_y_points = list()
-        for i in range(len(ranges)):
-            x_y_points.append( (ranges[i]*np.cos(i*scan.angle_increment + scan.angle_min), ranges[i]*np.sin(i*scan.angle_increment + scan.angle_min)) )
+        
+
+
+
+        # rospy.loginfo("Side: " + str(self.SIDE))
+        # rospy.loginfo("Scan Start Index: " + str(start_index))
+        # rospy.loginfo("Scan End Index: " + str(end_index))
+        # rospy.loginfo("Scan length: " + str(len(scan.ranges)))
+        # rospy.loginfo("Number of measurments" + str((scan.angle_max -  scan.angle_min)/scan.angle_increment))
+        # rospy.loginfo("Max range: " + str(scan.range_max))
+
 
         #generate a line from the (x,y) coordinates using least squares
-        x = [x_y_points[i][0] for i in range(len(x_y_points))]
-        y = [x_y_points[i][1] for i in range(len(x_y_points))]
-        A = np.vstack([x, np.ones(len(x))]).T
-        m, b = np.linalg.lstsq(A, y)[0]
+        # Approach 1
+        # A = np.vstack([x, np.ones(len(x))]).T
+        # m, b = np.linalg.lstsq(A, y)[0]
+
+        # Approach 2
+        # A = np.concatenate((x, np.ones_like(x)), axis=1)
+        # m, b = np.linalg.lstsq(A, y.T[0], rcond = -1)[0]
+
+        # x_ = np.linspace(0., 5., num=20)
+        # y_ = m*x_ + b   
+
+        # VisualizationTools.plot_line(x_, y_, self.line_pub, frame="/laser")
+
+        # b = np.abs(b)
+
+
+
+
+        # rospy.loginfo("points: " + str(x_y_points))
+        # rospy.loginfo("m: " + str(m))
+        # rospy.loginfo("Generating a line with this number of points: " + str(len(x)))
 
         # Generate error term
         # V1: find angle of line
         # angle = np.arctan(m)
+        # debug_angle = angle * 180 / np.pi # convert to degrees
         # position = angle
 
+        # rospy.loginfo("Angle: " + str(debug_angle))
+
+
         # V2: find distance between line and origin
-        distance = np.sqrt(m**2 + 1)
-        position = distance
+        # distance = np.sqrt(m**2 + 1)
+        # distance = np.sqrt((b/((m**2) + 1))**2 + (b/(m + (1/m)))**2)
+        # position = distance
+
+        # V3: Position is y intercept
+        # position = b
+
+        # V4: Position is minimum distance
+        if ranges[self.SIDE] <= 2:
+            position = ranges[self.SIDE]/2
+        else:
+            position = min(ranges)        
+
+        position = np.min(ranges)
+
+        # rospy.loginfo("Position: " + str(position))
+        
 
         return position
     
@@ -84,62 +141,98 @@ class WallFollower:
         # V1: Errror w/ angle
         # reference_signal = 1/2 * np.pi
 
-        # V2: Error w/ distance
-        reference_singal = self.DESIRED_DISTANCE
+        # V2: Error w/ distance (subtractin)
+        reference_signal = self.DESIRED_DISTANCE
+        error = reference_signal - output_signal
 
-        error = reference_singal - output_signal
+        # V3: Error w/ distance (ratio)
+        # error = reference_singal/output_signal
+
+        #experiment w/ clipping error
+        # error = min(1, (reference_singal - output_signal)/0.5)
+
         return error
-
-    def calculate_control_signal(self, error, k_p, k_i, k_d):
-        '''
-        Determines u(t)
-        Calculates the control signal for the wall follower
-        '''
-        response = self.pid(error, k_p, k_i, k_d)
-
-        # TODO: Determine if my sides are correct
-        return response * self.SIDE
 
 
     def drive(self, scan):
         '''
         Publishes the calculated drive command
         '''
-        # TODO: Set KP, KI, KD, error pull
+        # TODO: Dynamically set KP, KI, KD
+        self.K_P = rospy.get_param("wall_follower/k_p")
+        self.K_I = rospy.get_param("wall_follower/k_i")
+        self.K_D = rospy.get_param("wall_follower/k_d")
 
-        if self.SIDE < 0: # right
-            theta_start = (scan.angle_min + scan.angle_max)/2
-            thete_end = scan.angle_max
-        else: # left
-            theta_start = scan.angle_min
-            theta_end = (scan.angle_min + scan.angle_max)/2
+        rate = rospy.Rate(100) # 20hz #TODO: determine if this is correct
 
-        position = self.find_position(scan, theta_start, theta_end)
+        angle_array = np.arange(scan.angle_min, scan.angle_max, scan.angle_increment)
+        ranges = np.array(scan.ranges)
+
+
+
+        side_ranges = ranges[np.argwhere( (self.SIDE * angle_array > 0) & (self.SIDE * angle_array < 0.6 * np.pi))]
+        side_angles = angle_array[np.argwhere( (self.SIDE * angle_array > 0) & (self.SIDE * angle_array < 0.6 * np.pi))]
+
+        # rospy.loginfo("min angle in angle array: " + str(side_angles[0]))
+        # rospy.loginfo("max angle in angle array: " + str(side_angles[-1]))
+        # rospy.loginfo("Angle increments " + str(scan.angle_increment))
+        # rospy.loginfo(scan.angle_min)
+        # rospy.loginfo("Side: " + str(self.SIDE))
+        # rospy.loginfo("Scan Range: " + str(scan.angle_min) + " to " + str(scan.angle_max))
+        # rospy.loginfo("Theta Start: " + str(theta_start))
+        # rospy.loginfo("Theta End: " + str(theta_end))
+
+        position = self.find_position(side_ranges, side_angles)
         error = self.find_error(position)
-        control_signal = self.calculate_control_signal(error, self.K_P, self.K_I, self.K_D)
+
+        steering_offset = self.find_angle(side_ranges, side_angles)
+        control_signal = steering_offset + self.pid(error, self.K_P, self.K_I, self.K_D) 
+
+        # rospy.loginfo("Error: " + str(error))
+        # rospy.loginfo("Position: " + str(position))
+
+
         
         ack_msg = AckermannDriveStamped()
         ack_msg.header.stamp = rospy.Time.now()
-        ack_msg.header.frame_id = "base_link" # TODO: determine if this is correct
-        ack_msg.drive.steering_angle = control_signal * self.MAX_STEERING
         ack_msg.drive.speed = self.VELOCITY
-
-        
+        ack_msg.drive.acceleration = 0
+        ack_msg.drive.jerk = 0
+        ack_msg.drive.steering_angle_velocity = 0
+        ack_msg.drive.steering_angle = control_signal  
         self.pub.publish(ack_msg)
 
+            
 
-
-
-
-
+        rospy.loginfo("Steering angle: " + str(ack_msg.drive.steering_angle))
+        rospy.loginfo("------------------------------------")
 
         
-
-    
-    # TODO:
-    # Write your callback functions here.
+        
+        
 
 if __name__ == "__main__":
     rospy.init_node('wall_follower')
     wall_follower = WallFollower()
     rospy.spin()
+
+
+'''
+TA Help
+1 - How do I dynamically update parameters (KP, KI, KD)
+rosparam set k_p 2.0
+
+2 - How do I see debug statements?
+* Launch w/ roslaunch wall_follower wall_follower.launch
+
+3 - How do I move the car's starting location?
+* Use 2D pose to pull robot to location
+
+4 - Ensure everything is running
+Takeaways: Negative angles are on the RIGHT!!!
+
+5 - Use rostopic echo to check messages
+* roscore
+* roslaunch racecar_simulator simulate.launch
+* roslaunch wall_follower wall_follower.launch
+'''
